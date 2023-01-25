1e3d3eeb9575aeb76d32cb2834de5f71fbbdac4e
Iulian Puscasu <iulian.puscasu.mail@gmail.com>
2023-01-25 19:23:01 +0100

    update

--- provision/pacman-packages-list-update.sh
+++ provision/pacman-packages-list-update.sh
@@ -1,4 +1,4 @@
 # will create a list of all explicitly installed packages, except those in groups base-devel
-comm -23 <(pacman -Qqe | sort) <(pacman -Qq -g base-devel | sort | uniq) > pacman-packages-list-install.sh
+comm -23 <(pacman -Qqe | sort) <(pacman -Qq -g base-devel | sort | uniq) > pacman-packages-list.txt
 
 # note: install the packages from the list after installing base-devel
--- provision/vscode-extensions-list-update.sh
+++ provision/vscode-extensions-list-update.sh
@@ -1,4 +1,4 @@
 # will write a list of all installed extensions (enabled or not)
-code --list-extensions > vscode-extensions-list-install.sh
+code --list-extensions > vscode-extensions-list.txt
 
 # code --list-extensions | xargs -L 1 echo code --install-extension > vscode-extensions-list-install.sh
